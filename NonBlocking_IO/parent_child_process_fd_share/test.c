103a687a4214dd40145b42e03c5642149b66e490
Your Name <you@example.com>
2022-07-26 22:58:30 +0800

    update

--- NonBlocking_IO/parent_child_process_fd_share/test.c
+++ NonBlocking_IO/parent_child_process_fd_share/test.c
@@ -5,13 +5,15 @@
 int main(char* argv[]) {
     int fd = open("abc.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
     pid_t pid = fork();
-    write(fd, "xyz", 3);
     if (0 == pid) {
         sleep(1);
-        printf("%ld\n", lseek(fd, 0, SEEK_CUR));
+        printf("child fd offset before write %ld\n", lseek(fd, 0, SEEK_CUR));
+        write(fd, "xyz", 3);
+        printf("child fd offset after write %ld\n", lseek(fd, 0, SEEK_CUR));
     }
     else {
-        printf("%ld\n", lseek(fd, 0, SEEK_CUR));
+        write(fd, "xyz", 3);
+        printf("parent fd offset %ld\n", lseek(fd, 0, SEEK_CUR));
     }
     close(fd);
     return 0;
